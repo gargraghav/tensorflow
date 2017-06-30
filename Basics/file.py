f77a620a74d34567fad6b2dc85e77f25848667d3
Raghav Garg <gargraghav.96@gmail.com>
2017-06-30 18:37:37 +0530

    Update

---
+++ Basics/file.py
@@ -0,0 +1,2 @@
+dir=join(dirname(realpath(dirname(__file__))),'Files')
+filename = dir + 'Olympics2016.csv'
---
+++ Basics/image1.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from os.path import dirname, realpath, join
+
+dir=join(dirname(realpath(dirname(__file__))),'Images')
+filename=dir + "/MarshOrchid.jpg"
+img_data=mpimg.imread(filename)
+
+image=tf.placeholder("uint8",[None,None,3])
+slice=tf.slice(image,[1000,0,0],[3000,-1,-1])
+
+with tf.Session() as sess:
+    result=sess.run(slice, feed_dict={image:img_data})
+    print(result.shape)
+
+plt.imshow(result)
+plt.show()
